c807d04ff63328e2a351395bb1e6e88663e3c906
ken5scal <kengoscal@gmail.com>
2016-08-14 20:36:08 +0900

    Implemented synonyms searching program using thesaurus api

--- main.go
+++ main.go
@@ -4,11 +4,27 @@ import (
 	"os"
 	"github.com/ken5scal/thesaurus"
 	"bufio"
+	"log"
+	"fmt"
 )
 
 func main() {
 	apiKey := os.Getenv("BHT_APIKEY")
 	thesaurus := &thesaurus.BigHuge{APIKEy:apiKey}
-
 	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		word:= s.Text()
+		syns, err := thesaurus.Synonyms(word)
+		if err != nil {
+			log.Fatalf("Failed searching synonyms for %q: %v\n", word, err)
+		}
+
+		if len(syns) == 0 {
+			log.Fatalf("No synonyms found for %q\n", word)
+		}
+
+		for _, syn := range syns {
+			fmt.Println(syn)
+		}
+	}
 }
